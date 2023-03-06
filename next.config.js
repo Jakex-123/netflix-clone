93a88930f7c1228b0d0c19a5cdebead72b4ee1dc
Aditya Gupta <101881966+Jakex-123@users.noreply.github.com>
2023-03-06 14:53:42 +0530

    new

--- next.config.js
+++ next.config.js
@@ -5,6 +5,11 @@ const nextConfig = {
 
 module.exports = nextConfig
 module.exports = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   images: {
     domains: ['image.tmdb.org','rb.gy','localhost:3000','image.tmdb.org/t/p','image.tmdb.org/t/p/original','www.youtube.com'],
   },
